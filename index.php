390d1349ebb6656a2251d1ce252d333ca1835f2f
eoswins <yo0h0o@yahoo.com>
2013-10-12 16:56:19 -0400

    added index.php file

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+
+    <!-- <link rel="stylesheet" type="text/css" href="style.css">   -->
+  <title>
+    lztech
+  </title>
+
+  </head>
+  
+  <body>
+  	<form name="ip" method="POST" action="">
+  	IP: <input type="text" name="ip">
+    <input type="submit" value="Submit">
+    <input type="hidden" name="submitted" value="1">
+    </form>
+    
+  
+  </body>
+
+
+</html>
+
